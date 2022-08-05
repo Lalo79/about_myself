dc33cae95679a3f842b45780939506d320f3d80f
Sibo_Fulgencio <eduardo.pacheco@utexas.edu>
2022-08-04 22:51:41 -0500

    Added Sentiment Analysis Project

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+document.querySelectorAll('.sc01').forEach( item => {
+    item.addEventListener('click', event => {
+  
+      selection = event.target;
+      
+      document.location.assign('https://antonio-gmr.github.io/Project-1-Market-Mood-Swing/');
+
+  
+  
+    })
+})
